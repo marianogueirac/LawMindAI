31cf1ddcbfe5f0e3ebbc7428297851b294430bba
Djalma Márcio Vieira <marciovieira@yahoo.com.br>
2023-10-30 14:29:05 -0300

    Melhoria no upload e remoção de códigos de teste.

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import fitz
 import openai
 from flask_cors import CORS
 import sqlite3
-import json
-from datetime import datetime
+
 
 app = Flask(__name__)
 CORS(app)
@@ -20,6 +19,8 @@ login_manager.init_app(app)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['PERMANENT_SESSION_LIFETIME'] = 5  # Define o tempo em segundos (por exemplo, 5 segundos)
 Session(app)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Define o limite de tamanho de arquivo (aqui, 16 MB)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Define o tempo limite de solicitação (aqui, sem tempo limite)
 
 class User(UserMixin):
     def __init__(self, id):
@@ -37,7 +38,7 @@ def load_user(user_id):
         return User(user[0])
     return None
 def mychat(prompt):
-    openai.api_key = "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"
+    openai.api_key = "---------chave openai aqui------------"
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
@@ -140,105 +141,20 @@ def upload_file():
         else:
             error_message = "Nenhum arquivo enviado"
 
-    return render_template('chatbot.html', pdf_text=pdf_text, error_message=error_message)
+    return jsonify(pdf_text=pdf_text, error_message=error_message)
 
 
 @app.route('/respond', methods=['GET'])
 def chatbot():
     try:
         pergunta = request.args.get('pergunta')
-        usuario_id = 1
-        nome_sessao = "Sessao_" + datetime.now().strftime("%Y%m%d_%H%M%S")
-        data_hora = "2023-10-25 14:30:00"
-
-        conn = sqlite3.connect('sessoes_usuarios.db')
-        cursor = conn.cursor()
-
-        # Recupera as mensagens da sessão atual (ou cria uma nova sessão)
-        cursor.execute("SELECT id, mensagens FROM sessoes WHERE usuario_id = ? AND nome_sessao = ?",
-                       (usuario_id, nome_sessao))
-        registro = cursor.fetchone()
-
-        if registro:
-            # A sessão já existe, adiciona a pergunta e a resposta à sessão
-            sessao_id, mensagens_json = registro
-            mensagens = json.loads(mensagens_json)
-        else:
-            # A sessão não existe, cria uma nova sessão
-            cursor.execute("INSERT INTO sessoes (usuario_id, nome_sessao, data_hora, mensagens) VALUES (?, ?, ?, ?)",
-                           (usuario_id, nome_sessao, data_hora, "[]"))
-            conn.commit()
-            sessao_id = cursor.lastrowid
-            mensagens = []
-
-        # Adiciona a pergunta atual à sessão
-        mensagens.append({"tipo": "pergunta", "texto": pergunta})
-
-        # Concatena as mensagens para enviar ao modelo
-        mensagens_concatenadas = "\n\n".join([msg["texto"] for msg in mensagens])
-
-        resposta = mychat(mensagens_concatenadas)
-
-        # Adiciona a resposta à sessão
-        mensagens.append({"tipo": "resposta", "texto": resposta})
-
-        # Atualiza a sessão no banco de dados com todas as mensagens
-        cursor.execute("UPDATE sessoes SET mensagens = ? WHERE id = ?", (json.dumps(mensagens), sessao_id))
-        conn.commit()
-        conn.close()
+        resposta = mychat(pergunta)
 
     except Exception as e:
         resposta = 'Ops! Ocorreu um erro e não pude retornar a pergunta: {}'.format(e)
 
     return jsonify(resposta=resposta)
 
-@app.route('/get_session', methods=['GET'])
-def get_session():
-    nome_sessao = request.args.get('nome_sessao')
-    usuario_id = 1  # Substitue pelo ID do usuário real
-
-    # Recupera a sessão do banco de dados
-    conn = sqlite3.connect('sessoes_usuarios.db')
-    cursor = conn.cursor()
-    cursor.execute(
-        "SELECT * FROM sessoes WHERE usuario_id = ? AND nome_sessao = ?",
-        (usuario_id, nome_sessao)
-    )
-    sessao = cursor.fetchone()
-
-    # Se a sessão existe, retorna as mensagens
-    if sessao:
-        cursor.execute(
-            "SELECT tipo, texto FROM mensagens WHERE sessao_id = ?",
-            (sessao[0],)
-        )
-        mensagens = cursor.fetchall()
-
-        sessao_completa = {
-            "nome_sessao": sessao[3],
-            "mensagens": [{"tipo": msg[0], "texto": msg[1]} for msg in mensagens]
-        }
-        return jsonify(sessao=sessao_completa)
-    # Se a sessão não existe, cria uma sessão vazia
-    nova_sessao = {"nome_sessao": nome_sessao, "mensagens": []}
-    return jsonify(sessao=nova_sessao)
-
-@app.route('/get_sessions')
-def get_sessions():
-    conn = sqlite3.connect('sessoes_usuarios.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT nome_sessao FROM sessoes")
-    sessoes = [sessao[0] for sessao in cursor.fetchall()]
-    conn.close()
-    return jsonify(sessoes=sessoes)
-
-def convert_pdf_to_text(pdf_file_path):
-    pdf_document = fitz.open(pdf_file_path)
-    pdf_text = ""
-    for page_num in range(len(pdf_document)):
-        page = pdf_document[page_num]
-        pdf_text += page.get_text()
-    return pdf_text
 def convert_pdf_from_memory(pdf_file):
     # Abra o arquivo PDF diretamente da memória
     pdf_bytes = pdf_file.read()
